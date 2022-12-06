19be8b559397663a02b7222cdc0d8d1c2c183461
liuyunxiao <75168572@qq.com>
2022-12-07 00:36:53 +0800

    feat: 添加全局配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import * as dotenv from 'dotenv';
+import * as Joi from 'joi';
+
+const envFilePath = `.env.${process.env.NODE_ENV || 'development'}`;
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath,
+      load: [() => dotenv.config({ path: '.env' })],
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production')
+          .default('development'),
+        DB_TYPE: Joi.string()
+      })
+    }),
+    UserModule
+  ],
+  controllers: [],
+  providers: []
 })
 export class AppModule {}
---
+++ src/user/user.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ConfigEnum } from 'src/enum/config.enum';
+
+@Controller('user')
+export class UserController {
+  constructor(private configService: ConfigService) {}
+
+  @Get()
+  test() {
+    console.log('test api', this.configService.get(ConfigEnum.DB_TYPE));
+    console.log('test api', this.configService.get(ConfigEnum.DB_HOST));
+    console.log('test api', this.configService.get(ConfigEnum.DB_PASSWORD));
+
+    return 'Hello World!';
+  }
+}
